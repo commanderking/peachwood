b1a8337646614237f5799cefe11e7102a7153928
Jeffrey King <jking@hey.com>
2021-11-28 19:02:21 -0500

    fix positioning issues with footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import { Box } from "@chakra-ui/react";
 
 const Footer = () => {
   return (
-    <Box backgroundColor="yellow" flex={1} padding={20}>
+    <Box backgroundColor="yellow" padding={20}>
       <p>Peach's (and maybe Woody's) Commentary Here</p>
     </Box>
   );
