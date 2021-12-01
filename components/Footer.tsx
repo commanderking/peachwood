61ae6b78cd8f2d5473cb28ccdcafdd1a222f632f
Jeffrey King <jking@hey.com>
2021-12-01 18:27:55 -0500

    use generic image instead of avatar for quicker loads

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
-import { Box, Flex, Avatar, WrapItem } from "@chakra-ui/react";
-import { Image } from "data/images";
+import { Box, Flex, Image, WrapItem } from "@chakra-ui/react";
+import { Image as ImageType } from "data/images";
 type Props = {
-  currentImage: Image;
+  currentImage: ImageType;
 };
 
 const avatarBaseUrl = "/profile/";
@@ -9,9 +9,18 @@ const avatarBaseUrl = "/profile/";
 const Footer = ({ currentImage }: Props) => {
   return (
     <Flex backgroundColor="lightpink" padding={5} borderRadius={10}>
-      <Box display="flex" justifyContent="center" alignItems="center">
-        <Avatar
-          size="lg"
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        minWidth="50px"
+      >
+        <Image
+          width="50px"
+          height="50px"
+          style={{
+            borderRadius: "50px",
+          }}
           src={`${avatarBaseUrl}${currentImage.commentary[0].avatarImage}`}
         />
       </Box>
