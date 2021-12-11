f162d5d43785688d68cee6dfd9b77f10f2ff3681
Jeffrey King <jking@hey.com>
2021-12-10 22:02:02 -0500

    second round of code cleanup

---
+++ constants/comments.ts
@@ -0,0 +1,26 @@
+import { Comment } from "data/images";
+
+export const landingScreenComments: Comment[] = [
+    {
+      commentator: "PEACH",
+      avatarImage: "peach_thinking.png",
+      EN: "Meowllo! Peach here!! I'll be your host. I'd love to share some updates with you! Scroll down to see more photos of Woody and me!",
+      CH: "喵嘍！ Peach 在此！！ 我是這網頁的網主。 我想跟你分享今年的一些小消息! 想看到更多我跟 Woody 的照片， 請往下滑！",
+    },
+  ] 
+
+
+export const farewellComments: Comment[] = [
+    {
+      commentator: "PEACH",
+      avatarImage: "peach_thinking.png",
+      EN: "Thanks for taking the time to catch up with us! Special thanks to Uncle Jeffrey for putting this site together for us. If you are seeing this, it means you hold a special place in our family's heart in some way. And so we wish you happy holidays, and a happy and healthy New Year! 🎉",
+      CH: "感謝大家來跟我們回顧我們的2021年， 也非常感謝Jeffrey 叔叔為我們建立這網站。收到這卡片的人都是我們心中很重要的家人朋友。 希望大家過節快樂！ 新年快樂！",
+    },
+    {
+      commentator: "WOODY",
+      avatarImage: "woody_excited.png",
+      EN: "Woof! Nice to meet you, and see you next year! 🎊",
+      CH: "汪汪！ 很高興能夠見到你！ 明年見！",
+    },
+  ] 
