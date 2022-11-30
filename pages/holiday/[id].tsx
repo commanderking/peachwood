2ebbd3b1f5b4e38a7c869f26202ef8e7ab3aef1a
Jeffrey King <jking@hey.com>
2022-11-30 21:59:09 +0800

    add dynamic title

--- pages/holiday/[id].tsx
+++ pages/holiday/[id].tsx
@@ -1,5 +1,6 @@
 import { getImagesForYear, getIds } from "data/images";
-import { GetStaticPaths, GetStaticProps } from "next";
+import { GetStaticProps } from "next";
+import Head from "next/head";
 
 import { Box, Heading, Text } from "@chakra-ui/react";
 import { useState } from "react";
@@ -17,6 +18,9 @@ const YearlyImages = ({ yearlyData }: { yearlyData: YearlyData }) => {
 
   return (
     <Box margin="auto" maxWidth={[500, 600]}>
+      <Head>
+        <title>Peachwood {yearlyData.id} Holiday Card</title>
+      </Head>
       <Box pl={5} pr={5}>
         <Box
           height="100vh"
